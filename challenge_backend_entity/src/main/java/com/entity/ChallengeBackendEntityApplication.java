ec9a8fc2ed91835aca53a4ac45fac7c7fec78729
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2023-06-02 14:43:47 -0500

    Creando el microservicio de la tabla tb_entidad. Se realizó la conexión a la base de datos MySQL y mapeo de la entidad.

---
+++ challenge_backend_entity/src/main/java/com/entity/ChallengeBackendEntityApplication.java
@@ -0,0 +1,12 @@
+package com.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendEntityApplication.class, args);
+	}
+}
---
+++ challenge_backend_entity/src/main/java/com/entity/entity/EntityM.java
@@ -0,0 +1,37 @@
+package com.entity.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_entidad")
+public class EntityM {
+
+	@Id
+	@Column(name = "id_entidad")
+	public Long id;
+	
+	@Column(name = "id_tipo_documento")
+	public Long idTypeDocument;
+	
+	@Column(name = "nro_documento")
+	public String nroDocument;
+	
+	@Column(name = "nombre_comercial")
+	public String tradename;
+	
+	@Column(name = "id_tipo_contribuyente")
+	public Long idTypeContributor;
+
+	@Column(name = "direccion")
+	public String address;
+
+	@Column(name = "telefono")
+	public String telephone;
+
+	@Column(name = "estado")
+	public Boolean state;
+}
