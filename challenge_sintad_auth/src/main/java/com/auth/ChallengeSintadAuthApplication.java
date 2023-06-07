30cee4ba35f3d367ea896ccd1b401b615c2bdab4
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2023-06-07 14:41:14 -0500

    En autenticacióm, se agregó la configuración para permitir origins

--- challenge_sintad_auth/src/main/java/com/auth/ChallengeSintadAuthApplication.java
+++ challenge_sintad_auth/src/main/java/com/auth/ChallengeSintadAuthApplication.java
@@ -39,7 +39,7 @@ public class ChallengeSintadAuthApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
